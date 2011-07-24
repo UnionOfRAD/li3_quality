fdec6c08df163669d3d61ff253ebe133f696df3b
Michael Nitschinger <michael@nitschinger.at>
2011-07-24 21:22:11 +0200

    adding empty filter.

---
+++ extensions/test/filter/Syntax.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_quality\extensions\test\filter;
+
+/**
+ * 
+ */
+class Syntax extends \lithium\test\Filter {
+
+	/**
+	 *
+	 */
+	public static function apply($report, $tests, array $options = array()) {
+		return array();
+	}
+
+}
+
+?>
