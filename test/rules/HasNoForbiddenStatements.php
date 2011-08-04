201e99c203f9cda8769776dca4b127b44607159e
Michael Nitschinger <michael@nitschinger.at>
2011-08-04 14:15:43 +0200

    Adding forbidden statements rule.
    This rule checks for forbidden statements like goto or alternative syntax statements like endfor. It also checks for var_dump statements in the code. See the documentation for more details on this rule. This rule also combines many rules that were separated in lithium_qa.

---
+++ test/rules/HasNoForbiddenStatements.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_quality\test\rules;
+
+class HasNoForbiddenStatements extends \li3_quality\test\Rule {
+
+	protected $_forbidden = array(
+		'T_ENDDECLARE', 'T_ENDFOR', 
+		'T_ENDFOREACH', 'T_ENDIF',
+		'T_ENDSWITCH', 'T_ENDWHILE',
+		'T_PRINT', 'T_GOTO', 
+		'T_EVAL', 'T_GLOBAL'
+	);
+	
+	public function apply($testable) {
+		$tokens = $testable->tokens();
+		foreach($tokens as $token) {
+			if(in_array($token['name'], $this->_forbidden)) {
+				var_dump($tokens);
+				$token = strtolower(substr($token['name'], 2));
+				$this->addViolation(array(
+					'message' => 'Forbidden "' . $token . '" statement found',
+					'line' => $token['line']
+				));
+			}
+
+			if($token['name'] === 'T_STRING' && $token['content'] === 'var_dump') {
+				$this->addViolation(array(
+					'message' => 'Forbidden "var_dump" statement found',
+					'line' => $token['line']
+				));
+			}
+		}
+	}
+}
+
+?>
