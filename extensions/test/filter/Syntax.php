91b423756cb650fa93f4807ef3d52d475c5033b5
Michael Nitschinger <michael@nitschinger.at>
2011-08-09 15:35:54 +0200

    Adding test filter and basic output formatting.

--- extensions/test/filter/Syntax.php
+++ extensions/test/filter/Syntax.php
@@ -21,6 +21,27 @@ class Syntax extends \lithium\test\Filter {
 	 */
 	public static function apply($report, $tests, array $options = array()) {
 		
+		foreach ($tests->invoke('subject') as $class) {
+			$report->collect(__CLASS__, array(
+				$class => Rules::apply(new Testable(array('path' => $class)))
+			));
+		}
+		
+		return $tests;
+	}
+	
+	/**
+	 *
+	 */
+	public static function analyze($report, array $options = array()) {
+		$results = $report->results['filters'][__CLASS__];
+		$metrics = array();
+		foreach($results as $result) {
+			foreach($result as $class => $metric) {
+				$metrics[$class] = $metric['violations'];
+			}
+		}
+		return $metrics;
 	}
 
 }
--- extensions/test/templates/html/syntax.html.php
+++ extensions/test/templates/html/syntax.html.php
@@ -1,3 +1,31 @@
 <h3>Syntax</h3>
 
-<?php var_dump($data); ?>
+<?php foreach($data as $class => $violations): ?>
+<table class="metrics">
+	<thead>
+		<tr>
+			<th colspan="3"><?php echo $class; ?></th>
+		</tr>
+		<?php if($violations): ?>
+		<tr>
+			<th>Line</th>
+			<th>Position</th>
+			<th>Violation</th>
+		</tr>
+		<?php endif; ?>
+	</thead>
+	<tbody>
+		<?php if($violations): ?>
+			<?php foreach($violations as $violation): ?>
+			<tr>
+				<td><?php echo $violation['line']; ?></td>
+				<td><?php echo $violation['position']; ?></td>
+				<td><?php echo $violation['message']; ?></td>
+			</tr>
+			<?php endforeach; ?>
+		<?php else: ?>
+			<tr><td colspan="3">No violations found!</td></tr>
+		<?php endif; ?>
+	</tbody>
+</table>
+<?php endforeach; ?>
