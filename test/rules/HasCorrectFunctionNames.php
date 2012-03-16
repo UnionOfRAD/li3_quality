de1c562afcc3b56e2cacb0d66ff2c68cfdd9ef53
Michael Nitschinger <michael@nitschinger.at>
2012-03-16 23:24:19 +0100

    Adding HasCorrectFunctionNames

---
+++ test/rules/HasCorrectFunctionNames.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_quality\test\rules;
+
+use lithium\util\Inflector;
+
+class HasCorrectFunctionNames extends \li3_quality\test\Rule {
+
+	protected $_magicMethods = array(
+		'__construct', '__destruct', '__call',
+		'__callStatic', '__get', '__set',
+		'__isset', ' __unset', '__sleep',
+		'__wakeup', '__toString', '__invoke',
+		'__set_state', '__clone'
+	);
+
+	public function apply($testable) {
+		$tokens = $testable->tokens();
+
+		foreach($tokens as $key => $token)  {
+			if($token['name'] == 'T_FUNCTION') {
+				$lookahead = $tokens[$key+2];
+				if($lookahead['name'] == 'T_STRING' && !in_array($lookahead['content'], $this->_magicMethods)) {
+					$name = preg_replace('/^_+/', '', $lookahead['content']);
+					if($name != Inflector::camelize($name, false)) {
+						$this->addViolation(array(
+							'message' =>  'Function "' . $name . '" is not in camelBack style',
+							'line' => $lookahead['line']
+						));
+					}
+				}
+			}
+		}
+	}
+
+}
+
+?>
