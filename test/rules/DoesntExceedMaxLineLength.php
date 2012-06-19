3e917855e2e68c8e51279666618b1516979491dc
Michael Nitschinger <michael@nitschinger.at>
2012-06-19 10:39:18 +0200

    Making use of maximum line length variable and displaying it accordingly. thanks @kalnado

--- test/rules/DoesntExceedMaxLineLength.php
+++ test/rules/DoesntExceedMaxLineLength.php
@@ -13,14 +13,14 @@ use lithium\g11n\Multibyte;
 class DoesntExceedMaxLineLength extends \li3_quality\test\Rule {
 
 	public function apply($testable) {
-		$message = "Maximum line length exceeded";
 		$maxLength = 100;
 		$tabWidth  = 3;
+		$message = "Maximum line length of \"" . $maxLength . "\" exceeded";
 
 		foreach($testable->lines() as $i => $line) {
 			$tabBounty = substr_count($line, "\t") * $tabWidth;
 			$strlen = Multibyte::strlen($line, array('name' => 'li3_quality'));
-			if(($length = $tabBounty + $strlen) > 100) {
+			if(($length = $tabBounty + $strlen) > $maxLength) {
 				$this->addViolation(array(
 					'message' => $message,
 					'line' => $i+1,
