73bdc50469c181bfb5e5fb06cb4f23110ba68349
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-12-05 17:17:59 -0600

    Add correct short tag detection

---
+++ tests/cases/test/rules/HasCorrectInlineHTMLTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace li3_quality\tests\cases\test\rules;
+
+class HasCorrectInlineHTMLTest extends \li3_quality\test\Unit {
+
+	public $rule = 'li3_quality\test\rules\HasCorrectInlineHTML';
+
+	public function testCorrectInlineHTMLFormat() {
+		$code = <<<EOD
+<div>
+Name: <?=\$name; ?>
+</div>
+EOD;
+		$this->assertRulePass(array(
+			'source' => $code,
+			'wrap' => false,
+		), $this->rule);
+	}
+
+	public function testInlineHTMLWithNoSpacing() {
+		$code = <<<EOD
+<div>
+Name: <?=\$name;?>
+</div>
+EOD;
+		$this->assertRuleFail(array(
+			'source' => $code,
+			'wrap' => false,
+		), $this->rule);
+	}
+
+	public function testInlineHTMLWithNoSemicolonWithSpace() {
+		$code = <<<EOD
+<div>
+Name: <?=\$name ?>
+</div>
+EOD;
+		$this->assertRuleFail(array(
+			'source' => $code,
+			'wrap' => false,
+		), $this->rule);
+	}
+
+	public function testInlineHTMLWithNoSemicolonWithOutSpace() {
+		$code = <<<EOD
+<div>
+Name: <?=\$name?>
+</div>
+EOD;
+		$this->assertRuleFail(array(
+			'source' => $code,
+			'wrap' => false,
+		), $this->rule);
+	}
+
+	public function testInlineHTMLWithExtraSpace() {
+		$code = <<<EOD
+<div>
+Name: <?= \$name; ?>
+</div>
+EOD;
+		$this->assertRuleFail(array(
+			'source' => $code,
+			'wrap' => false,
+		), $this->rule);
+	}
+
+}
+
+?>
