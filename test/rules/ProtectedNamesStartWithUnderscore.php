6eb43e7bf4eb428a375360bf3b2cccade4a1f3f4
Blaine Schmeisser <BlaineSch@gmail.com>
2012-12-15 18:36:10 -0600

    Refactor rule validating protected names
    I also extended it to check variables which is in the rulebook under 'Member Visibility'

---
+++ test/rules/ProtectedNamesStartWithUnderscore.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_quality\test\rules;
+
+class ProtectedNamesStartWithUnderscore extends \li3_quality\test\Rule {
+
+	/**
+	 * Will iterate the tokens looking for protected methods and variables, once
+	 * found it will validate the name of it's parent starts with an underscore.
+	 *
+	 * @param  Testable $testable The testable object
+	 * @return void
+	 */
+	public function apply($testable) {
+		$tokens = $testable->tokens();
+		foreach ($tokens as $position => $token) {
+			if ($token['id'] === T_PROTECTED) {
+				$parentLabel = $tokens[$token['parent']]['label'];
+				if (substr($parentLabel, 0, 1) !== '_') {
+					$this->addViolation(array(
+						'message' => 'Protected method/variable "' . $parentLabel . '" does not ' .
+							'start with "_"',
+						'line' => $token['line']
+					));
+				}
+			}
+		}
+	}
+
+}
+
+?>
