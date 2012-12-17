1e556c7a3309cee71af9ac96725c8756c0aed010
Blaine Schmeisser <BlaineSch@gmail.com>
2012-12-16 20:37:52 -0600

    Add warnings to line length rule

--- test/rules/DoesntExceedMaxLineLength.php
+++ test/rules/DoesntExceedMaxLineLength.php
@@ -12,17 +12,27 @@ use lithium\g11n\Multibyte;
 
 class DoesntExceedMaxLineLength extends \li3_quality\test\Rule {
 
-	public function apply($testable) {
-		$maxLength = 100;
-		$tabWidth = 3;
-		$message = "Maximum line length of \"" . $maxLength . "\" exceeded";
+	public $config = array(
+		'maxLimit' => 100,
+		'softLimit' => 80,
+		'tabWidth' => 3,
+	);
 
+	public function apply($testable) {
+		extract($this->config);
 		foreach ($testable->lines() as $i => $line) {
 			$tabBounty = substr_count($line, "\t") * $tabWidth;
 			$strlen = Multibyte::strlen($line, array('name' => 'li3_quality'));
-			if (($length = $tabBounty + $strlen) > $maxLength) {
+			$totalLength = ($length = $tabBounty + $strlen);
+			if ($totalLength > $maxLimit) {
 				$this->addViolation(array(
-					'message' => $message,
+					'message' => "Maximum line length of {$maxLimit} exceeded",
+					'line' => $i + 1,
+					'position' => $length
+				));
+			} elseif ($totalLength >= $softLimit) {
+				$this->addWarning(array(
+					'message' => "Soft line length of {$softLimit} exceeded",
 					'line' => $i + 1,
 					'position' => $length
 				));
