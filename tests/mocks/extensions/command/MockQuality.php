bf82f65c6eaed9ed8d85658c97711d5181a9db9f
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-12-21 21:45:52 -0600

    Add broken quality tests to fix tomorrow

---
+++ tests/mocks/extensions/command/MockQuality.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace li3_quality\tests\mocks\extensions\command;
+
+class MockQuality extends \li3_quality\extensions\command\Quality {
+
+	public $pathConfig = array();
+
+	protected function _testables($options = array()) {
+		return $this->_filter(__METHOD__, $args, function($self, $args) {
+			$key = (isset($options['path'])) ? $options['path'] : $this->library;
+			if (isset($this->pathConfig[$key])) {
+				return $this->pathConfig[$key];
+			}
+			return parent::_testables($options);
+		});
+	}
+
+}
