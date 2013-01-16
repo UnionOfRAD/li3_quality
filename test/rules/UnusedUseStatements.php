a5584fee5cd8bde61c6e281b420a816c93509032
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2013-01-15 20:52:20 -0600

    Create rule for testing unused use statements.

---
+++ test/rules/UnusedUseStatements.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_quality\test\rules;
+
+class UnusedUseStatements extends \li3_quality\test\Rule {
+
+	/**
+	 * Will iterate over each line checking if tabs are only first
+	 *
+	 * @param  Testable $testable The testable object
+	 * @return void
+	 */
+	public function apply($testable, array $config = array()) {
+		$useStatements = array();
+		$regexArray = array();
+		$compiledRegex = '/$^/i';
+		$tokens = $testable->tokens();
+		$lines = $testable->lines();
+		$typeCache = $testable->typeCache();
+		$matches = array();
+		if (!isset($typeCache[T_USE])) {
+			return;
+		}
+		foreach ($typeCache[T_USE] as $tokenId) {
+			$token = $tokens[$tokenId];
+			$line = $lines[$token['line'] - 1];
+			if (preg_match('/^use (?:([^ ]+ as )|(.*\\\))?(.*);$/i', $line, $matches) === 1) {
+				$useStatements[strtolower($matches[3])] = $token['line'];
+				$regexArray[] = "{$matches[3]}";
+			}
+		}
+		$compiledRegex = $this->_compileRegex($regexArray);
+		foreach ($lines as $line) {
+			$foundUse = preg_match('/^use/', $line) === 1;
+			$foundStatement = preg_match_all($compiledRegex, $line, $matches) !== false;
+			if (!$foundUse && $foundStatement) {
+				foreach ($matches[0] as $match) {
+					if (preg_match('/^new ([^ ]+)$/', $match, $smallMatches) === 1) {
+						unset($useStatements[$smallMatches[1]]);
+					} elseif (preg_match('/^([^:]+)::$/', $match, $smallMatches) === 1) {
+						unset($useStatements[$smallMatches[1]]);
+					}
+				}
+				$compiledRegex = $this->_compileRegex($regexArray);
+			}
+		}
+		foreach ($useStatements as $useStatement => $line) {
+			$this->addViolation(array(
+				'message' => 'Class ' . $useStatement . ' was never called',
+				'line' => $line,
+			));
+		}
+	}
+
+	/**
+	 * Will compile a new regex to search each line for.
+	 *
+	 * The regex is compiled after every potential change to the regex array.
+	 *
+	 * @param  array  $regexArray
+	 * @return string
+	 */
+	protected function _compileRegex(array $regexArray = array()) {
+		return '/' . implode('|', $regexArray) . '/';
+	}
+
+}
+
+?>
---
+++ tests/cases/test/rules/UnusedUseStatementsTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace li3_quality\tests\cases\test\rules;
+
+class UnusedUseStatementsTest extends \li3_quality\test\Unit {
+
+	public $rule = 'li3_quality\test\rules\UnusedUseStatements';
+
+	public function testBasicStaticAndInstanceClasses() {
+		$code = <<<EOD
+use foo;
+use foo\bar;
+new bar();
+foo::baz();
+EOD;
+		$this->assertRulePass($code, $this->rule);
+	}
+
+	public function testAliasClass() {
+		$code = <<<EOD
+use foo as bar;
+new bar();
+EOD;
+		$this->assertRulePass($code, $this->rule);
+	}
+
+	public function testExtending() {
+		$code = <<<EOD
+use foo\bar\baz\MockTestable as Testable;
+use Baz;
+class Foo implements Baz extends Testable {
+}
+EOD;
+		$this->assertRulePass($code, $this->rule);
+	}
+
+	public function testCaseInsensitive() {
+		$code = <<<EOD
+use Baz;
+class Foo implements baz {
+}
+EOD;
+		$this->assertRulePass($code, $this->rule);
+	}
+
+	public function testSimpleUnused() {
+		$code = <<<EOD
+use Baz;
+EOD;
+		$this->assertRuleFail($code, $this->rule);
+	}
+
+	public function testUnusedAlias() {
+		$code = <<<EOD
+use foo\bar\baz\MockTestable as Testable;
+class Foo {
+}
+EOD;
+		$this->assertRuleFail($code, $this->rule);
+	}
+
+	public function testUseStatementsInHeredoc() {
+		$code = <<<EOD
+<<<EOT
+use foo\bar\baz\MockTestable as Testable;
+EOT;
+EOD;
+		$this->assertRulePass($code, $this->rule);
+	}
+
+}
+
+?>
