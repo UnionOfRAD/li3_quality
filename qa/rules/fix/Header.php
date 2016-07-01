f304a360e8199a9e8c4abb403c3b209e520db104
David Persson <davidpersson@gmx.de>
2016-07-01 10:33:45 +0200

    Update header rule and template.

--- qa/rules/fix/Header.php
+++ qa/rules/fix/Header.php
@@ -11,34 +11,46 @@ namespace li3_quality\qa\rules\fix;
 use lithium\core\Libraries;
 use lithium\util\Text;
 
+/**
+ * Adds missing PHP file headers and updates existing.
+ */
 class Header extends \li3_quality\qa\Rule {
 
 	public function enabled($testable, array $config = array()) {
-		return file_exists(Text::insert($config['template'], array(
-			'library' => Libraries::get(true, 'path')
-		)));
+		return $testable->isPHP() && file_exists($this->_template());
 	}
 
 	public function apply($testable, array $config = array()) {
 		$contents = $testable->source();
 		$contents = explode("\n", $contents);
 
-		$template = Text::insert($config['template'], array(
-			'library' => Libraries::get(true, 'path')
-		));
-		$template = Text::insert(file_get_contents($template), array(
+		$template = Text::insert(file_get_contents($this->_template()), array(
 			'year' => date('Y')
 		));
 
-		if (strpos($contents[1], '*') === false) {
-			$header = explode("\n", $template);
-
-			$one = array_shift($contents);
-			$contents = array_merge($header, $contents);
-			array_unshift($contents, $one);
-
-			$testable->source(implode("\n", $contents));
+		if ($contents[0] !== '<?php') {
+			return;
 		}
+		$one = array_shift($contents);
+		$header = explode("\n", $template);
+
+		if ($contents[0] === '/**') {
+			while($line = array_shift($contents)) {
+				if ($line === ' */') {
+					break;
+				}
+			}
+		}
+
+		$contents = array_merge($header, $contents);
+		array_unshift($contents, $one);
+		$testable->source(implode("\n", $contents));
+	}
+
+	protected function _template() {
+		return Text::insert($config['template'], array(
+			'cwd' => getcwd()
+		));
 	}
 }
 
